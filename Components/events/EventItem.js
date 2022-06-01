c5776e91c01c6d02d3c4df810959f9bba8c6b769
Robert <osipovitchr@gmail.com>
2022-06-01 11:51:28 -0400

    ui button + icon SVG components-- first page complete

--- Components/events/EventItem.js
+++ Components/events/EventItem.js
@@ -1,37 +1,49 @@
-import React from 'react'
-import Link from 'next/link'
-import classes from './EventItem.module.css'
+import React from "react";
+import Link from "next/link";
+import Button from "../ui/Button";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
+
+import classes from "./EventItem.module.css";
 
 const EventItem = ({ title, image, date, location, id }) => {
-    const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
-        day: 'numeric',
-        month: 'long',
-        year: 'numeric'
-    })
+  const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
 
-    const fomattedAddress = location.replace(', ', '\n')
+  const fomattedAddress = location.replace(", ", "\n");
 
-    const exploreLink = `/events/${id}`
+  const exploreLink = `/events/${id}`;
 
   return (
     <li className={classes.item}>
-        <img src={`/${image}`} alt={title} />
-        <div className={classes.content}>
-            <div className={classes.summary}>
-                <h2>{title}</h2>
-                <div className={classes.date}>
-                    <time>{humanReadableDate}</time>
-                </div>
-                <div className={classes.address}>
-                    <address>{fomattedAddress}</address>
-                </div>
-            </div>
-            <div className={classes.actions}>
-                <Link href={exploreLink}>Explore Event</Link>
-            </div>
+      <img src={`/${image}`} alt={title} />
+      <div className={classes.content}>
+        <div className={classes.summary}>
+          <h2>{title}</h2>
+          <div className={classes.date}>
+            <DateIcon />
+            <time>{humanReadableDate}</time>
+          </div>
+          <div className={classes.address}>
+            <AddressIcon />
+            <address>{fomattedAddress}</address>
+          </div>
         </div>
+        <div className={classes.actions}>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
+        </div>
+      </div>
     </li>
-  )
-}
+  );
+};
 
-export default EventItem
+export default EventItem;
